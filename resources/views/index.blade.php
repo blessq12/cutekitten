cb89d47118126bd97a18ca0876b44cb86116b851
Andrew <code70@inbox.ru>
2023-03-27 12:51:17 +0700

    index template

---
+++ resources/views/index.blade.php
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<base href="/">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CuteKitten | Детская одежда</title>
+	<meta name="description" content="">
+	<meta name="author" content="Andrew Korobkov">
+	<meta name="robots" content="noindex,nofollow">
+	<meta name="rating" content="safe for kids">
+	<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+	<link href="https://fonts.cdnfonts.com/css/comic-sans" rel="stylesheet">
+	<link rel="stylesheet" href="style.css">
+	<!-- favicon -->
+	<link rel="icon" href="favicon.svg">
+	<meta name="theme-color" content="#ffffff">
+	<link rel="manifest" href="manifest.json">
+    <style>
+        .content{
+            max-height: 100px;
+        }
+        svg{
+            max-height: 100px;
+            max-width: 100px;
+            fill: #000;
+            height: 100%;
+            width: 100%;
+        }
+        .text-content h1{
+            font-size: 40px;
+            font-family: 'comic sans';
+            font-weight: 800;
+            margin-bottom: 0;
+        }
+        .text-content h5{
+            font-size: 15px;
+        }
+        .logo{
+            margin-right: 15px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+	<div class="row align-items-center justify-content-center" style="min-height: 100vh;">
+		<div class="col-12 col-md-6 col-lg-6">
+			<div class="content d-flex justify-content-center align-items-center">
+				<div class="logo">
+					<svg id="Layer_2" data-name="Layer 2" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 522 522">
+						<defs>
+							<style>
+								.cls-1{fill:none;stroke:#000;stroke-miterlimit:10;stroke-width:10px;}
+							</style>
+						</defs>
+						<title>logo_vector</title>
+						<path d="M493.07,373.87h0a7.36,7.36,0,0,0-9.24-4.44C424.94,388.9,335.75,405.78,232,383.77c-1-14.89-1.36-29.93-1-44.81a468.36,468.36,0,0,1,5.94-62.6c37-13,61.94-30.59,76.72-42.91,16.59-13.83,25-24.78,30.69-34.48,4.83-8.19,24.82-42,11.37-67.07a38.48,38.48,0,0,0-15.16-15.53c-9.81-5.36-19.47-4.51-26.52-3.79-7.24.73-15.19,2.69-34.48,12.5a384.47,384.47,0,0,0-36,20.84c-3-6.33-15-29.26-41.68-39.78-5.3-2.09-21.2-8.11-40.92-3.79-6.66,1.46-22.4,4.9-33.34,18.94-8.6,11-9.87,23-10.61,29.93-.59,5.54-.94,15.1,2.65,28.42,10.39,38.48,41.93,64.79,48.5,70.1a161.6,161.6,0,0,0,53.88,28.5,481.58,481.58,0,0,0-5.45,60.34c-.38,13.89-.13,27.92.71,41.85a479.2,479.2,0,0,1-184-90.67,7.42,7.42,0,0,0-10.23,1A7.13,7.13,0,0,0,24,301,493.76,493.76,0,0,0,222.3,396.45,490.62,490.62,0,0,0,330.85,408.6a504.65,504.65,0,0,0,157.62-25.48A7.19,7.19,0,0,0,493.07,373.87Z" transform="translate(3.6 6.29)"/>
+						<rect class="cls-1" x="5" y="5" width="512" height="512"/>
+					</svg>
+				</div>
+				<div class="text-content">
+					<h1>CuteKitten</h1>
+					<h5>Одежда для самых важных</h5>
+				</div>
+			</div>
+			
+		</div>
+	</div>
+</div>
+<script type="text/javascript" src="https://code.jquery.com/jquery-3.6.3.min.js"></script>
+<script>
+	$(document).ready(function(){
+		
+	})
+</script>
+</body>
+</html>
